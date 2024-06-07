f454bacebd074063b163058eefc21bdcf591641c
Bell Eapen <github@gulfdoctor.net>
2024-06-06 19:40:41 -0500

    Refactor get_di function to include a default parameter
    This commit refactors the get_di function in the mydi.py file. It adds a default parameter to the function, allowing it to return the default value if the requested mydi is not found in the di dictionary. This change improves the flexibility and robustness of the function.

--- dhti_elixir_base/mydi.py
+++ dhti_elixir_base/mydi.py
@@ -1,9 +1,12 @@
 from kink import di
 
-def get_di(mydi: str):
+def get_di(mydi: str, default=None):
     _module = mydi.split("_", 1)[0]
     _mydi = mydi.split("_", 1)[1]
     try:
         return di[_module + "_" + _mydi]
     except KeyError:
-        return di[_mydi]
+        try:
+            return di[_mydi]
+        except KeyError:
+            return default
