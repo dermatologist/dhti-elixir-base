1ad1d8d911b3e6e3031d7324bdf4da064ca33f00
Bell Eapen <github_public@gulfdoctor.net>
2024-06-13 19:59:01 +0000

    feat: Add error handling to get_di function
    The get_di function in mydi.py has been updated to include error handling. If the requested mydi is not found in the di dictionary, the function now returns the default value specified in the new default parameter. This change improves the flexibility and robustness of the function.

--- dhti_elixir_base/mydi.py
+++ dhti_elixir_base/mydi.py
@@ -1,8 +1,11 @@
 from kink import di
 
 def get_di(mydi: str, default=None):
-    _module = mydi.split("_", 1)[0]
-    _mydi = mydi.split("_", 1)[1]
+    try:
+        _module = mydi.split("_", 1)[0]
+        _mydi = mydi.split("_", 1)[1]
+    except IndexError:
+        return di[mydi]
     try:
         return di[_module + "_" + _mydi]
     except KeyError:
