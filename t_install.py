cd4e1b514d7b4688c0ae48a81184c0fffe5387ac
dermatologist <github@gulfdoctor.net>
2025-09-19 19:00:22 -0500

    Update assertion to check result summary for accuracy

--- t_install.py
+++ t_install.py
@@ -8,5 +8,5 @@ from dhti_elixir_base.chain import BaseChain
 input = {"input": "Answer in one word: What is the capital of France?"}
 result = BaseChain().chain.invoke(input=input)  # type: ignore
 print(result)
-assert result == "Paris"
+assert result.summary == "Paris"
 
