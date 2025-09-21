124f67d6b2aef64f07e3a84cd9736db1139d0d18
Bell Eapen <github@gulfdoctor.net>
2025-09-21 16:04:55 -0500

    Add request parser functions and corresponding tests for CDS hooks

---
+++ src/dhti_elixir_base/cds_hook/request_parser.py
@@ -0,0 +1,44 @@
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# parse various cds hooks request formats
+def get_content_string_from_order_select(order_select):
+    if order_select.get("input"):
+        order_select = order_select.get("input")
+    entries = order_select.get("context", {}).get("draftOrders", {}).get("entry", [])
+    # if resourceType is CommunicationRequest, return the contentString from payload
+    for entry in entries:
+        resource = entry.get("resource", {})
+        if resource.get("resourceType") == "CommunicationRequest":
+            payload = resource.get("payload", [])
+            for item in payload:
+                content_string = item.get("contentString")
+                try:
+                    return json.loads(content_string)
+                except (json.JSONDecodeError, TypeError):
+                    return content_string
+    return None
+
+def get_patient_id_from_request(patient_view):
+    if patient_view.get("input"):
+        patient_view = patient_view.get("input")
+    patient_id = patient_view.get("context", {}).get("patientId")
+    if patient_id:
+        return patient_id
+    return None
+
+def get_context(input_data):
+    if input_data.get("input"):
+        input_data = input_data.get("input")
+    try:
+        context = input_data.get("context", None)
+    except AttributeError as e:
+        return input_data
+    try:
+        json.loads(context)
+        return context
+    except (TypeError, ValueError) as e:
+        return context
